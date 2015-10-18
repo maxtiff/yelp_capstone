c9980c42e8947118f52817612aaa486a8b5ce24a
Travis May <travismay@gmail.com>
2015-10-17 21:21:35 -0500

    Read in more json data

--- init_exploration.R
+++ init_exploration.R
@@ -1,1 +1,20 @@
 setwd('~/capstone')
+
+library(jsonlite)
+library(RJSONIO)
+library(rjson)
+
+## Source all required scripts.
+# required_scripts <- c('fsi_ifs_functions.R','corpus.R')
+# sapply(required_scripts, source, .GlobalEnv)
+
+
+review <- stream_in(file('data/yelp_academic_dataset_review.json'))
+
+business <- stream_in(file('data/yelp_academic_dataset_business.json'))
+
+business.attributes <- business$attributes
+
+tip <- stream_in(file('data/yelp_academic_dataset_tip.json'))
+
+user <- stream_in(file('data/yelp_academic_dataset_user.json'))
