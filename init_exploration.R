cd1b471ead7fc2e745acb14b4d43de0b6088f5ec
Travis <travismay@gmail.com>
2015-10-23 11:28:03 -0500

    Modify to use download function; use dynamic directory and file strucures

--- init_exploration.R
+++ init_exploration.R
@@ -5,11 +5,21 @@ library(RJSONIO)
 library(rjson)
 
 ## Source all required scripts.
-# required_scripts <- c('fsi_ifs_functions.R','corpus.R')
-# sapply(required_scripts, source, .GlobalEnv)
+required_scripts <- c('download.R')
+sapply(required_scripts, source, .GlobalEnv)
+
+## Download yelp dataset
+url      <- "https://d396qusza40orc.cloudfront.net/dsscapstone/dataset/yelp_dataset_challenge_academic_dataset.zip"
+download(url)
+
+## Get data directories
+dirs     <- list.dirs(dir(pattern = 'data'))
+
+## Get json files for analysis
+jsons    <- list.files(dirs[2],pattern = '\\.json$')
 
 ## Read in review json file
-review <- stream_in(file('data/yelp_academic_dataset_review.json'))
+review   <- stream_in(file('data/yelp_academic_dataset_review.json'))
 ## View row 1000 of review json
 review[100,]
 
@@ -20,20 +30,23 @@ business <- stream_in(file('data/yelp_academic_dataset_business.json'))
 prop.table(table(business$attributes$`Wi-Fi`))
 
 ## Read in tip json file
-tip <- stream_in(file('data/yelp_academic_dataset_tip.json'))
+tip      <- stream_in(file('data/yelp_academic_dataset_tip.json'))
 ## View row 1000 of tip json
 tip[1000,]
 
-## Read in user json file
-user <- stream_in(file('data/yelp_academic_dataset_user.json'))
-## Get user with complement votes for funny above 10k
-funny_dude <- user[which(user$compliments$funny >= 10000),]
+## Read in check-n json file
+checkin  <- stream_in(file())
 
-## Flatten from for cross tabulation
-cross <- flatten(user)
+## Read in user json file
+user     <- stream_in(file('data/yelp_academic_dataset_user.json'))
+## Get user with complement votes for funny above 10k
+fun_guy  <- user[which(user$compliments$funny >= 10000),]
+
+## Flatten user data for cross tabulation
+cross    <- flatten(user)
 ## Replace NAs with 0
 cross [is.na(cross)] <- 0
 ## Run fisher.test on cross tabulation
-results <- fisher.test(table(cross$fans >= 1, cross$compliments.funny >= 1))
+results  <- fisher.test(table(cross$fans >= 1, cross$compliments.funny >= 1))
 
 
